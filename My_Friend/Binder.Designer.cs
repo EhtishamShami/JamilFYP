5bfae9481427f98b95ab6eb4bdc52ff24cdc5318
Shami <theehtishamshami@live.com>
2016-02-09 01:24:32 +0500

    push the raja

---
+++ My_Friend/Binder.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace My_Friend
+{
+    partial class Binder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Pathbox = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.Voicebox = new System.Windows.Forms.TextBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(27, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 43);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Set Folder Destination";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(27, 110);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 43);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Bind Program";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(206, 191);
+            this.panel1.TabIndex = 2;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button3);
+            this.panel2.Controls.Add(this.Pathbox);
+            this.panel2.Location = new System.Drawing.Point(242, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(206, 274);
+            this.panel2.TabIndex = 3;
+            this.panel2.Visible = false;
+            // 
+            // Pathbox
+            // 
+            this.Pathbox.Location = new System.Drawing.Point(12, 110);
+            this.Pathbox.Multiline = true;
+            this.Pathbox.Name = "Pathbox";
+            this.Pathbox.Size = new System.Drawing.Size(168, 65);
+            this.Pathbox.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(105, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Set";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 91);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Set Path";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.Voicebox);
+            this.panel3.Controls.Add(this.button4);
+            this.panel3.Location = new System.Drawing.Point(12, 301);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(213, 152);
+            this.panel3.TabIndex = 4;
+            this.panel3.Visible = false;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(63, 102);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 0;
+            this.button4.Text = "Set Voice";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Voicebox
+            // 
+            this.Voicebox.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Voicebox.Location = new System.Drawing.Point(27, 40);
+            this.Voicebox.Multiline = true;
+            this.Voicebox.Name = "Voicebox";
+            this.Voicebox.Size = new System.Drawing.Size(162, 56);
+            this.Voicebox.TabIndex = 1;
+            // 
+            // Binder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(573, 465);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Binder";
+            this.Text = "Binder";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox Pathbox;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.TextBox Voicebox;
+        private System.Windows.Forms.Button button4;
+    }
+}
---
+++ My_Friend/Binder.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+namespace My_Friend
+{
+    public partial class Binder : Form
+    {
+        String path = ""; bool fol;
+        public Binder()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            fol = true;
+            panel1.Visible = false;
+            panel2.Visible = true;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            path = Pathbox.Text.ToString();
+            panel2.Visible = false;
+            panel3.Visible = true;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            bid(Voicebox.Text.ToString(),Pathbox.Text.ToString());
+            
+        }
+        void bid(String bind,String path)
+        {
+            String[] resultstring = {bind,path};
+            if (fol== true)
+            {
+                File.WriteAllLines("file.txt", resultstring);
+            }
+            My_Friend obj = new My_Friend();
+            obj.Show();
+            this.Close();
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            fol = true;
+            panel1.Visible = false;
+            panel2.Visible = true;
+        }
+    }
+}
