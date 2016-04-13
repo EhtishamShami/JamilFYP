f85d6180cb32c8c2032e472d8e6b50fe50efe8f1
Shami <theehtishamshami@live.com>
2016-04-12 21:09:14 -0700

    Upload Server class :)

---
+++ My_Friend/Server.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+
+namespace My_Friend
+{
+    public class Server
+    {
+        My_Friend obj = new My_Friend();
+        private TcpListener tcpListener;
+        public void Server1()
+        {
+
+            StartServer();
+            
+        }
+
+        public string myip()
+        {
+            IPHostEntry host;
+            string localIP = "?";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (IPAddress ip in host.AddressList)
+            {
+                if (ip.AddressFamily.ToString() == "InterNetwork")
+                {
+                    localIP = ip.ToString();
+                }
+            }
+            return localIP;
+        }
+        public bool StartServer()
+        {
+
+
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, 8080);
+                tcpListener.Start();
+                tcpListener.BeginAcceptTcpClient(new AsyncCallback(this.ProcessEvents), tcpListener);
+
+              //  Console.WriteLine("Listing at Port {0}.", 8080);
+            }
+            catch (Exception e)
+            {
+         //       Console.WriteLine(e.ToString());
+                return false;
+            }
+
+            return true;
+        }
+
+        private void ProcessEvents(IAsyncResult asyn)
+        {
+            try
+            {
+                TcpListener processListen = (TcpListener)asyn.AsyncState;
+                TcpClient tcpClient = processListen.EndAcceptTcpClient(asyn);
+                NetworkStream myStream = tcpClient.GetStream();
+                if (myStream.CanRead)
+                {
+                    StreamReader readerStream = new StreamReader(myStream);
+                    string myMessage = readerStream.ReadToEnd();
+                    //   Console.WriteLine("The Message is " + myMessage);
+                    obj.reply(myMessage);
+                    readerStream.Close();
+                }
+                myStream.Close();
+                tcpClient.Close();
+                tcpListener.BeginAcceptTcpClient(new AsyncCallback(this.ProcessEvents), tcpListener);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+
+
+    }
+}
---
+++ remotecontrol/app/src/main/java/com/example/ehtisham/remotecontrol/Client.java
@@ -0,0 +1,31 @@
+package com.example.ehtisham.remotecontrol;
+
+/**
+ * Created by Shami on 4/9/2016.
+ */
+import android.os.StrictMode;
+
+import java.io.PrintWriter;
+import java.net.Socket;
+
+
+public class Client {
+
+    public static void SendCommand(String command) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Socket socket;
+        try {
+            socket = new Socket( "192.168.10.39", 8080);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+            writer.print(command);
+            writer.flush();
+            writer.close();
+            socket.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
